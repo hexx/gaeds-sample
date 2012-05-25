afee93ffe3b4777fea4d393b5a3d6ba91723e401
The Hexx <greaterhexx@gmail.com>
2012-05-25 18:56:12 +0900

    コードを整理

--- src/main/scala/board.scala
+++ src/main/scala/board.scala
@@ -23,18 +23,16 @@ class MessageBoard extends unfiltered.filter.Plan {
   val messageLimit = 100
 
   def intent = {
-    case GET(Path("/")) => Ok ~> view
+    case GET(Path("/")) => view
     case POST(Path("/") & Params(params)) => {
       val m = new Message(params("name")(0), params("message")(0))
       if (Message.query.count >= messageLimit) {
         Datastore.delete(Message.query.sort(_.date asc).asSingleKey)
       }
       m.put()
-      Ok ~> view
+      view
     }
   }
 
-  def view = {
-    Ok ~> Twirl(html.index())
-  }
+  def view = Ok ~> Twirl(html.index())
 }
